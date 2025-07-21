5ad1137ee9e930513353cbbb02f43781975e10eb
vorittir <68040906@kmitl.ac.th>
2025-07-21 13:22:05 +0000

    lll

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = input("")
+number = int(number)
+if number==0:
+ print("this number is equal to zero.")
+if number <0:
+  print("this number is negattive.")
+if number >0:
+ print("this number is positive.")
+ print("")
